7eeaa3e18aad52405282ebe18ba97ef47dbe157f
tainarabaroni <baroni.tainara@escola.pr.gov.br>
2025-09-19 08:27:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,5 +9,45 @@ function mostraPalavrasChave(){
 }
 function processaTexto(texto){
    let palavras = texto.split(/\P{L}+/u);
-return palavras;
+
+   for (let i in palavras) {
+   palavras [i] = palavras[i].tolowerCase();
+   }
+
+palavras = tiraPalavrasRuins(palavras);
+
+const frequencias = contaFrequencias(palavras);
+let ordenadas = Object.keysy(frequencia).sort(ordenaPalavra);
+
+function ordenapalavras(p1,p2) {
+return frequencias [p2] - frequencia [p1];
+}
+
+console.log(ordenadas);
+return ordenadas.slice(0,10);
+}
+
+function contaFrequencias(palavras) {
+let frequencias = {};
+for (let i of palavras){
+   frequencias[i] = 0;
+   for (let j of palavras){
+      if (i==j) {
+         frequencias[i]++;
+      }
+
+   }
+}
+return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+   const PALAVRAS_RUINS = new Set(["para","uma","nós"]);
+   const palavrasBoas = [];
+   for (let palavra of palavras) {
+      if(!PALAVRAS_RUINS.has(palavras) && palavras.lenght > 2) {
+         palavrasBoas.push(palavras);
+      }
+   }
+   return palavrasBoas;
 }
